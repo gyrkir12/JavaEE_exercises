5a2791243dc25bc6dd76869bd55d61df8f910b88
Jonas Jensen <jonas.jensen@msn.com>
2016-05-02 09:43:18 +0200

    add News entity

---
+++ src/main/java/no/jenjon13/reeddit/data/entities/News.java
@@ -0,0 +1,84 @@
+package no.jenjon13.reeddit.data.entities;
+
+import no.jenjon13.reeddit.data.entities.embeddables.Score;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+public class News {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @Size(max = 64)
+    private String title;
+
+    @NotNull
+    @Size(max = 5000)
+    private String content;
+
+    @NotNull
+    @Past
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date timestamp;
+
+    @NotNull
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private SiteUser siteUser;
+
+    @Embedded
+    private Score score = new Score();
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public SiteUser getSiteUser() {
+        return siteUser;
+    }
+
+    public void setSiteUser(SiteUser siteUser) {
+        this.siteUser = siteUser;
+    }
+
+    public Score getScore() {
+        return score;
+    }
+
+    public void setScore(Score score) {
+        this.score = score;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
