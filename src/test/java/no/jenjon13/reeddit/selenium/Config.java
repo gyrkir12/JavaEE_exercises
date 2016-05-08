80a9f33d57fcda15b5b2cc296314da085162bf59
Jonas Jensen <jonas.jensen@msn.com>
2016-05-08 21:12:40 +0200

    added simple selenium load verification testing

---
+++ src/test/java/no/jenjon13/reeddit/selenium/Config.java
@@ -0,0 +1,12 @@
+package no.jenjon13.reeddit.selenium;
+
+public final class Config {
+    public static final String FIREFOX_BINARY_PATH = "D:\\Firefox\\firefox.exe";
+    public static final String HOST = "localhost";
+    public static final String CONTEXT = "/";
+    public static final String PORT = "8080";
+    public static final String INDEX = "";
+
+    private Config() {
+    }
+}
